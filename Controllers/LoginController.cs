344f9e63fcdc6e600de57b21b7fd58138fff6429
Rahul <Rahul.sharma@winshuttle.com>
2023-02-07 19:10:39 +0530

    aadding  files

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using BussinessLayer.Models;
 using BussinessLayer.BussinessUtil;
+using System.Web.Security;
+
 namespace EntityFrameworkDemo.Controllers
 {
     public class LoginController : BaseController
@@ -24,15 +26,22 @@ namespace EntityFrameworkDemo.Controllers
             return View(ViewBag.Data);
         }
         [HttpPost]
+        
         public ActionResult Authenticate([Bind(Include = "UserName,Password")] LoginModel loginModel)
         {
             
            bool found =  LoginUtility.Authenticate(loginModel);
-            if(!found)
+            if (!found)
             {
                 ViewBag.ErrorInvalid = "USer Name and Password does not match";
                 return View(ViewBag.Data);
             }
+            else
+            {
+                FormsAuthentication.SetAuthCookie(loginModel.UserName, true);
+                return RedirectToAction("Index", "Home");
+            }
+
             return View();
         }
        
